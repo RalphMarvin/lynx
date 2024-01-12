60de827a64d056a963353a2f6209d66b1f78ef88
Ralph Marvin <ralphmarvin4@gmail.com>
2024-01-12 11:37:25 +0000

    Replace fovea project checking with laravel project checking

--- src/common.ts
+++ src/common.ts
@@ -1,11 +1,15 @@
+import File from "fs-extra";
+
 export class Common {
   /**
-   * Checks and verifies that the current working directory is a fovea project.
+   * Checks and verifies that the current working directory is a laravel project.
+   * @returns
    */
-  static verifyFoveaProject(): boolean {
-    const foveaProjectRoot = process.cwd();
-    const index = foveaProjectRoot.lastIndexOf('/');
-    const projectName = foveaProjectRoot.substring(index);
-    return projectName.toLowerCase() === 'fovea';
+  static verifyIsLaravelProject(): boolean {
+    // Basically checking to see if an artisan file is present. This isn't the best
+    // way but this will do for now as a much faster solution.
+    const laravelProjectRoot = process.cwd();
+    const artisanFile = `${laravelProjectRoot}/artisan`;
+    return File.existsSync(artisanFile);
   }
 }
